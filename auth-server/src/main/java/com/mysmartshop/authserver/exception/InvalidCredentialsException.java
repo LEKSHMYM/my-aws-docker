43affcfebb3e4e38d7f03f6435c00cda4529bf69
LEKSHMYM <98512897+LEKSHMYM@users.noreply.github.com>
2023-07-26 12:25:48 +0530

    Add files via upload

---
+++ auth-server/src/main/java/com/mysmartshop/authserver/exception/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package com.mysmartshop.authserver.exception;
+
+public class InvalidCredentialsException extends RuntimeException{
+  public InvalidCredentialsException(String message) {
+	 super(message);
+ }
+}
